b3dec534b009dd63eb3eae9bc3d61f2dbb91eaca
akashpj123 <akashpj77@gmail.com>
2024-08-01 15:31:39 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,36 @@
+const BUTTONSS = document.querySelector('.BUTTONSS');
+
+BUTTONSS.addEventListener('click', function onClick() {
+    BUTTONSS.style.backgroundColor = 'blue';
+  BUTTONSS.style.color = 'white';
+});
+const wok = document.querySelector('.wok');
+
+wok.addEventListener('click', function onClick() {
+    wok.style.backgroundColor = 'blue';
+    wok.style.color = 'white';
+});
+const num1 = document.querySelector('.num1');
+
+num1.addEventListener('click', function onClick() {
+    num1.style.backgroundColor = 'blue';
+    num1.style.color = 'white';
+});
+const bet = document.querySelector('.bet');
+
+bet.addEventListener('click', function onClick() {
+    bet.style.backgroundColor = 'blue';
+    bet.style.color = 'white';
+});
+const week = document.querySelector('.week');
+
+week.addEventListener('click', function onClick() {
+    week.style.backgroundColor = 'blue';
+    week.style.color = 'white';
+});
+const wokkoe = document.querySelector('.wokkoe');
+
+wokkoe.addEventListener('click', function onClick() {
+    wokkoe.style.backgroundColor = 'blue';
+    wokkoe.style.color = 'white';
+});
